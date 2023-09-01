c1e3089cb56b86cd3c0f7c401853b53b406bc2dd
VitalijKo <vitalijko4805go@gmail.com>
2023-09-01 15:00:09 +0400

    Initial commit

---
+++ main.mjs
@@ -0,0 +1,101 @@
+import { Demodulator } from './demodulator.mjs';
+
+let readSamples = true;
+let button = document.querySelector('button');
+let introSection = document.querySelector('.intro');
+let mainSection = document.querySelector('.app');
+let waitingMessage = document.querySelector('.blink-me');
+let msgString = '';
+let msgsArray = [];
+let started = false;
+let msgReceived = false;
+let msgIndex = 0;
+let previousIndex;
+let timer;
+
+const demodulator = new Demodulator();
+
+async function start() {
+    const sdr = await RtlSdr.requestDevice();
+    introSection.style.display = 'none';
+    mainSection.style.display = 'block';
+
+    await sdr.open({ ppm: 0.5 });
+
+    const actualSampleRate = await sdr.setSampleRate(2000000);
+    const actualCenterFrequency = await sdr.setCenterFrequency(1090000000);
+
+    await sdr.resetBuffer();
+
+    while (readSamples) {
+        if (!started) {
+            console.log('starting...');
+
+            started = true;
+        }
+
+        const samples = await sdr.readSamples(128000);
+        const data = new Uint8Array(samples);
+
+        demodulator.process(data, 256000, onMsg)
+    }
+}
+
+const onMsg = (msg) => {
+    if (!msgReceived) {
+        waitingMessage.style.display = 'none';
+        msgReceived = true;
+    }
+
+    displayAircraftData(msg);
+}
+
+
+const displayAircraftData = msg => {
+    let message = msg;
+
+    msgsArray.push(JSON.stringify(message));
+
+    handleData(msgsArray);
+}
+
+
+const handleData = array => {
+    if (msgIndex !== previousIndex) {
+        let msg = JSON.parse(array[msgIndex]);
+        let keys = Object.keys(msg);
+
+        keys = keys.filter(k => k !== 'msg');
+
+        keys.map(k => {
+            msgString += `${k}: ${msg[k]},`;
+        });
+
+        showText('.data', msgString, 0, 20);
+
+        previousIndex = msgIndex;
+    }
+}
+
+var showText = function(target, message, index, interval) {
+    if (index < message.length) {
+        document.querySelector('.data').append(message[index++]);
+
+        if (message[index] === ',') {
+            document.querySelector('.data').append(message[index++]);
+            document.querySelector('.data').innerHTML += '</br>';
+        }
+
+        document.querySelector('.data').scrollTop = document.querySelector('.data').scrollHeight;
+
+        timer = setTimeout(function() {
+            showText(target, message, index, interval);
+        }, interval);
+    } else {
+        clearTimeout(timer);
+
+        ++msgIndex;
+    }
+}
+
+button.onclick = () => start();
